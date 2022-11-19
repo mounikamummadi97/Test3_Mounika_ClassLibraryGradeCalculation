e9f7f0182d0e3c5c69cd9deb540034a31857a153
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-11-19 15:08:39 +0530

    Add project files.

---
+++ Test3_Mounika_ClassLibraryGradeCalculation/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace Test3_Mounika_ClassLibraryGradeCalculation
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public int Standard { get; set; }
+
+
+
+        public class Student_Info
+        {
+            public int StudentID { get; set; }
+            public string? StudentName { get; set; }
+            public int Standard { get; set; }
+
+
+
+            public void SetStudentDetails()
+            {
+                Console.WriteLine("Enter Student ID");
+                StudentID = int.Parse(Console.ReadLine());
+
+
+
+                Console.WriteLine("Enter Student Nme");
+                StudentName = Console.ReadLine();
+
+
+
+                Console.WriteLine("Enter Student Standard");
+                Standard = int.Parse(Console.ReadLine());
+            }
+
+
+
+            public void DisplayStudentDetails()
+            {
+                Console.WriteLine("Student ID : " + StudentID);
+                Console.WriteLine("Student Nme : " + StudentName);
+                Console.WriteLine("Student Standard : " + Standard);
+            }
+        }
+    }
---
+++ Test3_Mounika_ClassLibraryGradeCalculation/Marks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3_Mounika_ClassLibraryGradeCalculation
+{
+    public interface Student_Marks
+    {
+        public int Telugu { get; set; }
+        public int Hindi { get; set; }
+        public int English { get; set; }
+        public int Maths { get; set; }
+        public int Science { get; set; }
+        public int Social { get; set; }
+
+
+
+        public abstract void SetStudentMarks();
+        public abstract void DisplayStudentMarks();
+
+
+
+    }
+}
